cadda044d498894a0552be3b91635dfa6b70a119
Liam Härter Leão <119792380+liamharter@users.noreply.github.com>
2023-03-10 16:17:36 -0300

    Update login.php

--- login.php
+++ login.php
@@ -2,15 +2,15 @@
 session_start();
 session_destroy();
 session_start();
-$_SESSION['login'] = $_GET['login'];
-$_SESSION['senha'] = MD5($_GET['senha']);;
+$_SESSION['login'] = $_POST['login'];
+$_SESSION['senha'] = MD5($_POST['senha']);;
 
 $connect = mysqli_connect('estoquesites.mysql.dbaas.com.br','estoquesites','Estoque@123');
 $db = mysqli_select_db($connect, 'estoquesites') or die(mysqli_error($connect));
 
 $resultado =mysqli_query($connect, "SELECT login FROM usuarios WHERE login='".$_SESSION['login']."' ORDER BY RAND() LIMIT 1");
 $row = mysqli_fetch_array($resultado);
-if ($_SESSION['login'] == $_POST['GET']){
+if ($_SESSION['login'] == $_POST['POST']){
     echo( "olá $_SESSION[login]" );
 }
 
