a02d392ba94659c2370dd39f22f70cfbba66a1b6
Liam Härter Leão <119792380+liamharter@users.noreply.github.com>
2023-03-10 14:57:39 -0300

    Add files via upload

---
+++ login.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+session_destroy();
+session_start();
+$_SESSION['login'] = $_POST['login'];
+$_SESSION['senha'] = MD5($_POST['senha']);;
+
+$connect = mysqli_connect('estoquesites.mysql.dbaas.com.br','estoquesites','Estoque@123');
+$db = mysqli_select_db($connect, 'estoquesites') or die(mysqli_error($connect));
+
+$resultado =mysqli_query($connect, "SELECT login FROM usuarios WHERE login='".$_SESSION['login']."' ORDER BY RAND() LIMIT 1");
+$row = mysqli_fetch_array($resultado);
+if ($_SESSION['login'] == $_POST['login']){
+    echo( "olá $_SESSION[login]" );
+}
+
+
+
+?>
+
